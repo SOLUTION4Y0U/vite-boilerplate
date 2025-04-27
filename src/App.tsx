5688d0bfe952f380a9f1177322a13af651bafbbe
daniil <daniil@MacBook-Air-Daniil.local>
2025-04-27 21:07:38 +0300

    0

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       </div>
       <h1>TWA + Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count: number) => count + 1)}>
           count is {count}
         </button>
       </div>
